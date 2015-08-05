b26508da0eadb9f27f7fbbc514bc7497dcb9fb87
Hpaulson <technicalhannah@gmail.com>
2015-08-05 14:49:39 -0600

    added activity and xml for cheat page

---
+++ app/src/main/java/com/example/a712948/geoquiz/CheatActivity.java
@@ -0,0 +1,17 @@
+package com.example.a712948.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * @author Hannah Paulson
+ * @since 8/5/15.
+ */
+public class CheatActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
