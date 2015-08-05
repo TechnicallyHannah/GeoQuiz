68ca475d5a16c5489a537f1d8155daa489acdf5c
Hpaulson <technicalhannah@gmail.com>
2015-08-05 14:23:16 -0600

    things look better in landscape mode now

--- app/src/main/java/com/example/a712948/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/a712948/geoquiz/QuizActivity.java
@@ -2,13 +2,19 @@ package com.example.a712948.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -48,6 +54,7 @@ public class QuizActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -60,49 +67,106 @@ public class QuizActivity extends ActionBarActivity {
             }
         });
 
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkAnswer(false);
-            }
 
-        });
+        mFalseButton.setOnClickListener(new View.OnClickListener()
 
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+                                        {
+                                            @Override
+                                            public void onClick(View view) {
+                                                checkAnswer(false);
+                                            }
 
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+                                        }
 
-            }
-        });
+        );
 
-        mBackButton = (ImageButton) findViewById(R.id.back_button);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                if (mCurrentIndex < 0) {
-                    mCurrentIndex = mQuestionBank.length;
-                } else {
-                    updateQuestion();
-                }
-            }
-        });
+        mQuestionTextView = (TextView)
+
+                findViewById(R.id.question_text_view);
+
+        mNextButton = (ImageButton)
+
+                findViewById(R.id.next_button);
+
+        mNextButton.setOnClickListener(new View.OnClickListener()
+
+                                       {
+                                           @Override
+                                           public void onClick(View view) {
+                                               mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                                               updateQuestion();
+
+                                           }
+                                       }
+
+        );
+
+        mBackButton = (ImageButton)
+
+                findViewById(R.id.back_button);
+
+        mBackButton.setOnClickListener(new View.OnClickListener()
+
+                                       {
+                                           @Override
+                                           public void onClick(View view) {
+                                               mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                                               if (mCurrentIndex < 0) {
+                                                   mCurrentIndex = mQuestionBank.length;
+                                               } else {
+                                                   updateQuestion();
+                                               }
+                                           }
+                                       }
+
+        );
 
         TextView tv = (TextView) findViewById(R.id.question_text_view);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+        tv.setOnClickListener(new View.OnClickListener()
+                              {
+                                  @Override
+                                  public void onClick(View view) {
+                                      mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                                      updateQuestion();
 
-            }
-        });
+                                  }
+                              }
+
+        );
+
         updateQuestion();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() Called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
     }
 
     @Override
