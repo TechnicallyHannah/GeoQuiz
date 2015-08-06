f4638c49edd1003e11861a5fff4f43d9380567cc
Hpaulson <technicalhannah@gmail.com>
2015-08-06 14:43:14 -0600

    completed challenege 1 , true false persists

--- app/src/main/java/com/example/a712948/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/a712948/geoquiz/CheatActivity.java
@@ -3,6 +3,7 @@ package com.example.a712948.geoquiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,9 +16,14 @@ public class CheatActivity extends ActionBarActivity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.example.a712948.geoquiz.answer_is_true";
     public static final String EXTRA_ANSWER_SHOWN = "com.example.a712948.geoquiz.answer_shown";
+    public static final String CHEATER_KEY = "is_cheater";
+    public static final String TAG = "tag";
+
     private boolean mAnswerIsTrue;
+    private boolean mIsCheater;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
@@ -37,6 +43,17 @@ public class CheatActivity extends ActionBarActivity {
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
         setAnswerShownResult(false);
 
+        if (savedInstanceState != null) {
+            mIsCheater = savedInstanceState.getBoolean(CHEATER_KEY, mIsCheater);
+            if (mIsCheater) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        }
+        Log.i("BEFORE ON CLICK", String.valueOf(mIsCheater));
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,7 +63,17 @@ public class CheatActivity extends ActionBarActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                mIsCheater = true;
+                Log.i("AFTER ON CLICK", String.valueOf(mIsCheater));
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        Log.i("In ON Instance State ", String.valueOf(mIsCheater));
+        savedInstanceState.putBoolean(CHEATER_KEY, mIsCheater);
     }
 }
